4237d25e511c1d2c4c09e3f412bf07d0e71edd42
emilyaand <104448537+emilyaand@users.noreply.github.com>
2022-06-02 21:42:15 +0400

    Add files via upload

---
+++ Task2 DownCasting, boxing and unboxing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _220602_classwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle vehicle = new Vehicle();
+            Car car = (Car) vehicle;
+            int a = 5;
+            object obj = a;
+            int b = (int)obj;
+
+        }
+            
+        public class Vehicle
+        {
+
+        }
+            
+        sealed class Car : Vehicle
+        {
+           
+        }
+       
+    }
+}
