c43e62044c4fbaeb4ac06a4e1f2b32e37d2146d4
emilyaand <104448537+emilyaand@users.noreply.github.com>
2022-06-02 21:15:46 +0400

    Add files via upload

---
+++ 220602 classwork, task1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _220602_classwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {    
+            string Response;
+            long[] arr = new long[0];
+
+            do
+            {
+                Console.WriteLine("Please enter a number");
+                string ededStr = Console.ReadLine();              
+                long number = long.Parse(ededStr);
+                Array.Resize(ref arr, arr.Length + 1);
+                arr[arr.Length - 1] = number;
+
+                Console.WriteLine("Would you like add a number, enter 'y' if yes and 'n' if no");
+                Response = Console.ReadLine();
+
+            } while (Response == "y");
+
+            foreach(var longItem in arr)
+            {
+                checked
+                {
+                    try
+                    {
+                        int a = (int)longItem;
+                    }
+                    catch
+                    {
+                        count++;
+                    }
+                }
+                int a = (int)longItem;
+            }
+            
+        }
+    }
+}
